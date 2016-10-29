ISI-TechKnAcq/techknacq-core

commit c4162e893ec0479e1cab4fb6a2449df03faaa941

Update citationgraph.java

src/main/java/edu/isi/techknacq/topics/readinglist/citationgraph.java
@@ -27,12 +27,14 @@ public class citationgraph {
     public citationgraph(){
         this.paperids=new HashMap<String, Integer>(100000);
         this.papernames=new ArrayList<String>(100000);
-        mycited=new ArrayList[500000];
         for(int i=0;i<mycited.length;i++)
             mycited[i]=new ArrayList<Integer>(10);
     }
     public void settopicnum(int _tnum){
         tnum=_tnum;
+    }
+    public void setmaxfilenum(int _fnum){
+        mycited=new ArrayList[_fnum];
     }
     public void Readcitation(String filename){
         try {

commit 88ebb6bd53b69a51214aa3a0b8d4e8d948f5b93e

Fix bug introduced in c4162e8, where the citationgraph initializer would assign values to a member before it was initialized.

src/main/java/edu/isi/techknacq/topics/readinglist/citationgraph.java
@@ -27,8 +27,6 @@ public class citationgraph {
     public citationgraph() {
         this.paperids = new HashMap<String, Integer>(100000);
         this.papernames = new ArrayList<String>(100000);
-        for (int i = 0; i < mycited.length; i++)
-            mycited[i] = new ArrayList<Integer>(10);
     }
 
     public void settopicnum(int _tnum) {
@@ -37,6 +35,8 @@ public class citationgraph {
 
     public void setmaxfilenum(int _fnum) {
         mycited = new ArrayList[_fnum];
+        for (int i = 0; i < mycited.length; i++)
+            mycited[i] = new ArrayList<Integer>(10);
     }
 
     public void Readcitation(String filename) {