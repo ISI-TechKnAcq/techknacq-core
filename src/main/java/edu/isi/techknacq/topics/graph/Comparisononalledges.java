103df3f276fbde5c5e7701a667cd4ee165f52a6e
Jonathan Gordon <jgordon@jonathangordon.org>
2017-03-24 16:42:50 -0700

    Clean.

--- src/main/java/edu/isi/techknacq/topics/graph/Comparisononalledges.java
+++ src/main/java/edu/isi/techknacq/topics/graph/Comparisononalledges.java
@@ -1,8 +1,5 @@
 package edu.isi.techknacq.topics.graph;
 
-
-import infodynamics.measures.continuous.kernel.EntropyCalculatorKernel;
-//import infodynamics.measures.discrete.MutualInformationCalculator;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -14,6 +11,9 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import infodynamics.measures.continuous.kernel.EntropyCalculatorKernel;
+
 import edu.isi.techknacq.topics.readinglist.Concept2doc;
 import edu.isi.techknacq.topics.readinglist.citationgraph;
 import edu.isi.techknacq.topics.topic.Indexpair;
@@ -36,18 +36,18 @@ public class Comparisononalledges {
     ArrayList<Double> topicscores;
 
     public Comparisononalledges() {
-        entropy=new EntropyCalculatorKernel();
+        entropy = new EntropyCalculatorKernel();
         entropy.initialise();
     }
 
     public void Readkey(String filename) {
-        ReadWeightedTopicKey myreader=new ReadWeightedTopicKey();
+        ReadWeightedTopicKey myreader = new ReadWeightedTopicKey();
         myreader.read(filename,5);
-        keynames=myreader.Getkeynames();
+        keynames = myreader.Getkeynames();
         myreader.Concepttowords(filename);
-        conceptsinword=myreader.Getconceptinword();
-        tnum=this.keynames.size();
-        flowmatrics=new double[tnum][tnum];
+        conceptsinword = myreader.Getconceptinword();
+        tnum = this.keynames.size();
+        flowmatrics = new double[tnum][tnum];
 
         // Now that we know the number of topics, introduce default topic
         // scores in case they're not provided.
@@ -59,7 +59,7 @@ public class Comparisononalledges {
 
     public void Readtopicscore(String filename) {
         try {
-            Scanner sc=new Scanner(new File(filename));
+            Scanner sc = new Scanner(new File(filename));
             while(sc.hasNext()){
                 //System.out.println(sc.next());
                 topicscores.add(sc.nextDouble());
@@ -71,17 +71,17 @@ public class Comparisononalledges {
 
     public void Extract(List a1, double []v1) {
         Arrays.fill(v1,0.0);
-        for(int i=0;i<a1.size();i++){
-            Weightpair o=(Weightpair)a1.get(i);
-            v1[o.getindex()]=o.getweight();
+        for (int i = 0; i < a1.size(); i++){
+            Weightpair o = (Weightpair)a1.get(i);
+            v1[o.getindex()] = o.getweight();
         }
     }
 
     public void Extract2(List a1, double []v1) {
-        Arrays.fill(v1,0.0);
-        for(int i=0;i<a1.size();i++){
-            Indexpair o=(Indexpair)a1.get(i);
-            v1[o.getindex()]=o.getweight();
+        Arrays.fill(v1, 0.0);
+        for (int i = 0; i < a1.size(); i++){
+            Indexpair o = (Indexpair)a1.get(i);
+            v1[o.getindex()] = o.getweight();
         }
     }
 
@@ -89,7 +89,7 @@ public class Comparisononalledges {
         int c = 0;
         int i = 0;
         int j = 0;
-        while (i < v1.length && j < v2.length){
+        while (i < v1.length && j < v2.length) {
             if (v1[i] > 0.0000000001 && v2[j] > 0.00000000001) {
                 c++;
             }
@@ -99,100 +99,108 @@ public class Comparisononalledges {
         return c;
     }
 
-    public double entropy(double p){
-        if(p>0.0)
-            return -p*Math.log(p);
+    public double entropy(double p) {
+        if (p > 0.0)
+            return -p * Math.log(p);
         else
             return 0.0;
     }
 
+    // Adapted from cc.mallet.util.
+    // Returns the KL divergence, K(p1 || p2).
     public double klDivergence(double[] p1, double[] p2) {
-      double klDiv = 0.0;
+        double klDiv = 0.0;
 
-      for (int i = 0; i < p1.length; ++i) {
-        if (p1[i] < 0.000001) { continue; }
-        if (p2[i] <0.0000001) { continue; } // Limin
+        for (int i = 0; i < p1.length; ++i) {
+            if (p1[i] < 0.000001) { continue; }
+            if (p2[i] < 0.0000001) { continue; } // Limin
 
-      klDiv += p1[i] * Math.log( p1[i] / p2[i] );
-      }
+            klDiv += p1[i] * Math.log( p1[i] / p2[i] );
+        }
 
-      return klDiv / Math.log(2); // moved this division out of the loop -DM
+        return klDiv / Math.log(2);
     }
+
     public double GetDiffSim(double []v1, double []v2){
-        double res=0.0;
-         int nzero1=0;
-        int nzero2=0;
-        int a=0;
-        int b=0;
-        int i=0;
-        int j=0;
-        while(i<v1.length&&j<v2.length){
-            if(v1[i]>0.0000000001)
+        double res = 0.0;
+        int nzero1 = 0;
+        int nzero2 = 0;
+        int a = 0;
+        int b = 0;
+        int i = 0;
+        int j = 0;
+        while (i < v1.length && j < v2.length) {
+            if (v1[i] > 0.0000000001)
                 nzero1++;
-            if(v2[j]>0.0000000001)
+            if (v2[j] > 0.0000000001)
                 nzero2++;
-            if(v1[i]>0.0000000001&&v2[j]>0.00000000001){
-               if(v1[i]>v2[j]){
-                   a++;
-               }else
-                   b++;
+            if (v1[i] > 0.0000000001 && v2[j] > 0.00000000001) {
+                if (v1[i] > v2[j]) {
+                    a++;
+                } else
+                    b++;
             }
             i++;
             j++;
         }
-        while(i<v1.length){
-            if(v1[i]>0.0000000001)
+        while (i < v1.length){
+            if (v1[i] > 0.0000000001)
                 nzero1++;
             i++;
         }
-        while(j<v2.length){
-            if(v2[j]>0.0000000001)
+        while (j < v2.length){
+            if (v2[j] > 0.0000000001)
                 nzero2++;
             j++;
         }
-        res=(double)((double)(a-b)*this.Getoccu(v1, v2)/(nzero1+nzero2));
+        res = (double)((double)(a-b)*this.Getoccu(v1, v2)/(nzero1+nzero2));
         return res;
     }
-    public double Getentropy(double []v1, double []v2){
+
+    public double Getentropy(double []v1, double []v2) {
         double ce;
         //System.out.println("res "+res);
         entropy.setObservations(v1);
-        ce=entropy.computeAverageLocalOfObservations();
+        ce = entropy.computeAverageLocalOfObservations();
         entropy.setObservations(v2);
-        ce-=entropy.computeAverageLocalOfObservations();
+        ce -= entropy.computeAverageLocalOfObservations();
         //System.out.println();
-        ce+=this.klDivergence(v1, v2);
-        ce-=this.klDivergence(v2, v1);
+        ce += this.klDivergence(v1, v2);
+        ce -= this.klDivergence(v2, v1);
         //System.out.println("ce "+ce);
         return ce;
     }
+
     public double Topsim(double []v1, double []v2){
         double res;
         int i;
-        int a=0;
-        int b=0;
-        for(i=0;i<v1.length;i++){
-            if(v1[i]>0.0000000001)
+        int a = 0;
+        int b = 0;
+        for (i = 0; i < v1.length; i++){
+            if (v1[i] > 0.0000000001)
                 a++;
         }
-        for(i=0;i<v2.length;i++){
-            if(v2[i]>0.0000000001){
+        for (i = 0; i < v2.length; i++){
+            if (v2[i] > 0.0000000001) {
                 b++;
             }
         }
-        int cooc=this.Getoccu(v1, v2);
-        if(a+b-cooc>0)
-            res=(double)cooc/(a+b-cooc);
+        int cooc = this.Getoccu(v1, v2);
+        if (a + b - cooc > 0)
+            res = (double)cooc/(a + b - cooc);
         else
-            res=0.0;
+            res = 0.0;
         return res;
     }
-    public void Run(String filename, int K, String citationfile, int maxfilewordnum){
+
+    public void Run(String filename, int K, String citationfile,
+                    int maxfilewordnum) {
         try {
             Concept2doc doc = new Concept2doc();
             doc.Initnum(tnum);
             doc.GettopK(K, filename);
-            conceptsindoc=doc.GetTopic2doc();
+            conceptsindoc = doc.GetTopic2doc();
+
             int cooccount;
             double informationflow;
             //double CEdoc;
@@ -203,81 +211,87 @@ public class Comparisononalledges {
             double hierdoc;
             double hierword;
             double citewang;
-            double []v1=new double[maxfilewordnum];
-            double []v2=new double[maxfilewordnum];
-            double []v3=new double[maxfilewordnum];
-            double []v4=new double[maxfilewordnum];
-            BufferedWriter out = null;
-            FileWriter fstream = null;
-            fstream = new FileWriter("alledge.tsv", false);
-            out = new BufferedWriter(fstream);
+            double []v1 = new double[maxfilewordnum];
+            double []v2 = new double[maxfilewordnum];
+            double []v3 = new double[maxfilewordnum];
+            double []v4 = new double[maxfilewordnum];
+
+            FileWriter fstream = new FileWriter("alledge.tsv", false);
+            BufferedWriter out = new BufferedWriter(fstream);
             out.write("sid\ts_topic\ttid\tt_topic\tsim_doc\tsim_word\tinformation_flow\tCE\tcitation\tHier\tCitation_Wang\n");
-            citationgraph mycite=new citationgraph();
+
+            citationgraph mycite = new citationgraph();
             mycite.settopicnum(tnum);
             mycite.setmaxfilenum(maxfilewordnum);
-           // mycite.Readcitation("acl.txt");
             mycite.Readcitation(citationfile);
             mycite.Readc2d(filename);
-            double [][]t2t=mycite.Computecitationlinks();
-            FileWriter fstream2=new FileWriter("entropy1.txt",false);
-            BufferedWriter out2=new BufferedWriter(fstream2);
-            FileWriter fstream3=new FileWriter("entropy2.txt",false);
-            BufferedWriter out3=new BufferedWriter(fstream3);
+            double [][]t2t = mycite.Computecitationlinks();
+
+            //FileWriter fstream2 = new FileWriter("entropy1.txt", false);
+            //BufferedWriter out2 = new BufferedWriter(fstream2);
+            //FileWriter fstream3 = new FileWriter("entropy2.txt", false);
+            //BufferedWriter out3 = new BufferedWriter(fstream3);
 
             for (int i = 0; i < tnum; i++) {
                 if (this.topicscores.get(i) < 0.42)
                     continue;
-                Extract(conceptsindoc[i],v1);
-                Extract2(conceptsinword[i],v3);
-                for(int j=0;j<tnum;j++){
+                Extract(conceptsindoc[i], v1);
+                Extract2(conceptsinword[i], v3);
+                for (int j = 0; j < tnum; j++){
                     if (this.topicscores.get(j) < 0.42)
                         continue;
-                    if(j==i)
+                    if (j == i)
                         continue;
-                    Extract(conceptsindoc[j],v2);
-                    Extract2(conceptsinword[j],v4);
-                    cooccount=this.Getoccu(v1, v2);
-                    if(cooccount>2){
-                        topicsim=this.Topsim(v1, v2);
-                        wordsim=this.Topsim(v3, v4);
+                    Extract(conceptsindoc[j], v2);
+                    Extract2(conceptsinword[j], v4);
+                    cooccount = this.Getoccu(v1, v2);
+                    if (cooccount > 2) {
+                        topicsim = this.Topsim(v1, v2);
+                        wordsim = this.Topsim(v3, v4);
                         //System.out.println(flowmatrics[i][j]);
-                        if(flowscores[i]>flowscores[j])
-                            informationflow=-this.flowmatrics[i][j];
+                        if (flowscores[i] > flowscores[j])
+                            informationflow =- this.flowmatrics[i][j];
                         else
-                            informationflow=this.flowmatrics[i][j];
-                        //CEdoc=this.Getentropy(v1, v2);
-                        CEword=this.Getentropy(v3, v4);
-                        hierdoc=this.GetDiffSim(v1, v2);
-                        hierword=this.GetDiffSim(v3, v4);
-                        cocite=(t2t[i][j]-t2t[j][i])/this.Getoccu(v1, v2);
-                        CEword=CEword/1.9514+hierword/1.1667;
-                        citewang=t2t[i][j];
-                        if(Math.abs(CEword)>0.007){
-                            if(CEword>0){
-                                out2.write(i+"\t"+j+"\t"+CEword+"\n");
-                            }else{
-                                out2.write(j+"\t"+i+"\t"+(0-CEword)+"\n");
-                            }
-                        }
-                        if(Math.abs(cocite)>10){
-                            if(cocite>0){
-                                out3.write(i+"\t"+j+"\t"+cocite+"\n");
-                            }else{
-                                out3.write(j+"\t"+i+"\t"+(0-cocite)+"\n");
-                            }
-                        }
-                        out.write(i+"\t"+this.keynames.get(i)+"\t"+j+"\t"+this.keynames.get(j)+"\t"+topicsim+"\t"+wordsim+"\t"+informationflow+"\t");
-                        out.write(CEword+"\t"+cocite+"\t");
-                        out.write(hierdoc+"\t"+citewang+"\n");
-                        if(i==55||i==59||i==82||i==172){
-                            System.out.print(i+"\t"+j+"\t"+wordsim+"\t"+informationflow+"\t");
-                            System.out.println(CEword+"\t"+cocite+"\t"+hierdoc+"\t"+citewang);
+                            informationflow = this.flowmatrics[i][j];
+                        //CEdoc = this.Getentropy(v1, v2);
+                        CEword = this.Getentropy(v3, v4);
+                        hierdoc = this.GetDiffSim(v1, v2);
+                        hierword = this.GetDiffSim(v3, v4);
+                        cocite = (t2t[i][j]-t2t[j][i])/this.Getoccu(v1, v2);
+                        CEword = CEword/1.9514 + hierword/1.1667;
+                        citewang = t2t[i][j];
+
+                        // if (Math.abs(CEword) > 0.007){
+                        //     if (CEword > 0){
+                        //         out2.write(i+"\t"+j+"\t"+CEword+"\n");
+                        //     } else {
+                        //         out2.write(j+"\t"+i+"\t"+(0-CEword)+"\n");
+                        //     }
+                        // }
+                        // if (Math.abs(cocite) > 10){
+                        //     if (cocite > 0){
+                        //         out3.write(i+"\t"+j+"\t"+cocite+"\n");
+                        //     } else {
+                        //         out3.write(j+"\t"+i+"\t"+(0-cocite)+"\n");
+                        //     }
+                        // }
+                        out.write(i + "\t" + this.keynames.get(i) + "\t" +
+                                  j + "\t" + this.keynames.get(j) + "\t" +
+                                  topicsim + "\t" + wordsim + "\t" +
+                                  informationflow + "\t" +
+                                  CEword + "\t" + cocite + "\t" +
+                                  hierdoc + "\t" + citewang + "\n");
+                        if (i % 10 == 0) {
+                            System.out.println(i + "\t" + j + "\t" + wordsim +
+                                               "\t" + informationflow + "\t" +
+                                               CEword + "\t" + cocite + "\t" +
+                                               hierdoc + "\t" + citewang);
                         }
                     }
                 }
             }
-            out2.close();
-            out3.close();
+            //out2.close();
+            //out3.close();
             out.close();
         } catch (IOException ex) {
             Logger.getLogger(Comparisononalledges.class.getName()).log(Level.SEVERE, null, ex);
@@ -286,7 +300,7 @@ public class Comparisononalledges {
 
     public static void main(String []args) {
         if (args.length < 1){
-            System.out.println("Usage [keyfile] [tree file] [topic composition file] [# topics] [citation file] [flow file] ([topicscorefile] [maxfilewordnum])");
+            System.out.println("Usage [keyfile] [tree file] [topic composition file] [K] [citation file] [flow file] ([topicscorefile] [maxfilewordnum])");
             System.exit(2);
         }
         Comparisononalledges alledge = new Comparisononalledges();
--- src/main/java/edu/isi/techknacq/topics/readinglist/Keyword2concept.java
+++ src/main/java/edu/isi/techknacq/topics/readinglist/Keyword2concept.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package edu.isi.techknacq.topics.readinglist;
 
 import java.io.BufferedReader;
@@ -22,60 +16,60 @@ import edu.isi.techknacq.topics.topic.Weightpair;
 import edu.isi.techknacq.topics.topic.WordPair;
 
 /**
- *
  * @author linhong
  */
 public class Keyword2concept {
     ArrayList<String> topics;
     ArrayList<ArrayList<WordPair>> wordintopic;
     ArrayList<Weightpair> hittopics;
-    int k=8;
-    public Keyword2concept(){
-        
+    int k = 8;
+
+    public Keyword2concept() {
+
     }
-    public void Readkey(String filename){
+
+    public void Readkey(String filename) {
         try {
-            this.topics=new ArrayList<String>(200);
-            this.wordintopic=new ArrayList(200);
-            FileInputStream fstream1 = null;
-            fstream1 = new FileInputStream(filename);
+            this.topics = new ArrayList<String>(200);
+            this.wordintopic = new ArrayList(200);
+            FileInputStream fstream1 = new FileInputStream(filename);
             // Get the object of DataInputStream
             DataInputStream in1 = new DataInputStream(fstream1);
             BufferedReader br = new BufferedReader(new InputStreamReader(in1));
             String strline;
-            int line=0;
-            while((strline=br.readLine())!=null){
-                 wordintopic.add(new ArrayList<WordPair>(20));
-                 Scanner sc=new Scanner(strline);
+            int line = 0;
+            while ((strline = br.readLine()) != null) {
+                wordintopic.add(new ArrayList<WordPair>(20));
+                Scanner sc = new Scanner(strline);
                 sc.useDelimiter("\t| ");
                 sc.next();
-                String name="";
+                String name = "";
                 String tempword;
-                float value=0.0f;
-                while(sc.hasNext()){
-                    tempword=sc.next();
-                    if((!tempword.contains(name)&&!name.contains(tempword))||name.length()<1){
-                        name+=tempword;
-                        name+=" ";
-
+                float value = 0.0f;
+                while (sc.hasNext()) {
+                    tempword = sc.next();
+                    if ((!tempword.contains(name) && !name.contains(tempword))
+                        || name.length() < 1){
+                        name += tempword;
+                        name += " ";
                     }
-                    if(sc.hasNext())
-                        value=sc.nextFloat();
-                wordintopic.get(line).add(new WordPair(tempword,value));
+                    if (sc.hasNext())
+                        value = sc.nextFloat();
+                    wordintopic.get(line).add(new WordPair(tempword, value));
                 }
                 this.topics.add(name);
                 line++;
             }
-            for(int i=0;i<wordintopic.size();i++){
-                float sum=0;
-                for(int j=0;j<wordintopic.get(i).size();j++){
-                     WordPair o=wordintopic.get(i).get(j);
-                     sum+=o.getprob();
+            for (int i = 0; i < wordintopic.size(); i++) {
+                float sum = 0;
+                for (int j = 0; j < wordintopic.get(i).size(); j++) {
+                     WordPair o = wordintopic.get(i).get(j);
+                     sum += o.getprob();
                 }
-                for(int j=0;j<wordintopic.get(i).size();j++){
-                    WordPair o=wordintopic.get(i).get(j);
-                    float oldv=o.getprob();
-                    o.setProb(oldv/sum);
+                for (int j = 0; j < wordintopic.get(i).size(); j++) {
+                    WordPair o = wordintopic.get(i).get(j);
+                    float oldv = o.getprob();
+                    o.setProb(oldv / sum);
                     wordintopic.get(i).set(j,o );
                 }
             }
@@ -86,46 +80,50 @@ public class Keyword2concept {
             Logger.getLogger(Keyword2concept.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    public ArrayList<Integer> Getmatch(String keyword){
-        hittopics=new ArrayList<Weightpair>(20);
-        for(int i=0;i<wordintopic.size();i++){
-            double hitcount=0;
-            for(int j=0;j<wordintopic.get(i).size()&&j<k;j++){
-                WordPair o=wordintopic.get(i).get(j);
-                if(o.getWord().length()<3)
+
+    public ArrayList<Integer> Getmatch(String keyword) {
+        hittopics = new ArrayList<Weightpair>(20);
+        for (int i = 0; i < wordintopic.size(); i++) {
+            double hitcount = 0;
+            for (int j = 0; j < wordintopic.get(i).size() && j < k; j++){
+                WordPair o = wordintopic.get(i).get(j);
+                if (o.getWord().length() < 3)
                     continue;
-                if(keyword.toLowerCase().indexOf(o.getWord())>=0||o.getWord().indexOf(keyword.toLowerCase())>=0){
-                    double lengthbonus=(double)Math.abs(keyword.length()-o.getWord().length())/Math.max(keyword.length(),o.getWord().length());
-                    lengthbonus=1-lengthbonus;
-                    hitcount+=o.getprob()*lengthbonus;
+                if (keyword.toLowerCase().indexOf(o.getWord()) >= 0 ||
+                    o.getWord().indexOf(keyword.toLowerCase()) >= 0) {
+                    double lengthbonus = (double)Math.abs(keyword.length() - o.getWord().length())/Math.max(keyword.length(),o.getWord().length());
+                    lengthbonus = 1 - lengthbonus;
+                    hitcount += o.getprob() * lengthbonus;
                 }
             }
-            if(hitcount>0){
-                hittopics.add(new Weightpair(hitcount,i));
+            if (hitcount > 0) {
+                hittopics.add(new Weightpair(hitcount, i));
             }
         }
         Collections.sort(hittopics);
-        ArrayList<Integer> topicindex=new ArrayList<Integer>(hittopics.size());
+        ArrayList<Integer> topicindex = new ArrayList<Integer>(hittopics.size());
         for (Weightpair o : hittopics) {
             topicindex.add(o.getindex());
         }
         return topicindex;
     }
-    
-    public ArrayList<String> Gettopics(){
+
+    public ArrayList<String> Gettopics() {
         return topics;
     }
-    public ArrayList<ArrayList<WordPair>> Getweighttopic(){
+
+    public ArrayList<ArrayList<WordPair>> Getweighttopic() {
         return this.wordintopic;
     }
-    public static void main(String []args){
-        Keyword2concept mykeyword=new Keyword2concept();
+
+    public static void main(String []args) {
+        Keyword2concept mykeyword = new Keyword2concept();
         mykeyword.Readkey("mallet-weighted-key.txt");
-        ArrayList<Integer> hits=mykeyword.Getmatch("machine_translation");
-        ArrayList<String> mytopic=mykeyword.Gettopics();
-        for(int i=0;i<hits.size();i++){
-            int index=hits.get(i);
-           System.out.println(mytopic.get(index));
+        ArrayList<Integer> hits = mykeyword.Getmatch("machine_translation");
+        ArrayList<String> mytopic = mykeyword.Gettopics();
+        for (int i = 0; i < hits.size(); i++) {
+            int index = hits.get(i);
+            System.out.println(mytopic.get(index));
         }
     }
 }
